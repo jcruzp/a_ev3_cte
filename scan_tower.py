9adee80cb4f08614bced0fb3cce3574498a4277a
Jose Cruz <joseacruzp@gmail.com>
2019-11-18 01:30:21 -0400

    New algoritm to scan tower

--- scan_tower.py
+++ scan_tower.py
@@ -3,6 +3,7 @@
 # twitter: @joseacruzp
 from enum import Enum
 from time import sleep
+import threading
 
 from ev3dev2.sound import Sound
 from ev3dev2.led import Leds
@@ -10,7 +11,13 @@ from ev3dev2.motor import OUTPUT_B, LargeMotor, SpeedPercent
 
 from ev3dev2.sensor.lego import UltrasonicSensor, GyroSensor
 
-
+class TowerPosition(Enum):
+    """
+    List of wheel position
+    """
+    LEFT = "Left"
+    RIGHT = "Right"
+    CENTER = "Center"
 
 class ScanTower():
     """
@@ -21,43 +28,60 @@ class ScanTower():
 
         self.scan_tower = LargeMotor(OUTPUT_B)
         self.gyro_sensor = GyroSensor()
+        self.gyro_sensor.reset
         self.ultrasonic_sensor = UltrasonicSensor()
         self.leds = Leds()
         self.sound = Sound()
+        self.tower_position = TowerPosition.CENTER
         
 
     def turn_left(self):
         #0..126
+        self.tower_position = TowerPosition.LEFT
         self.leds.set_color("LEFT", "RED")
-        self.scan_tower.on_for_degrees(SpeedPercent(20), -4, brake=True, block=False)
+        #self.scan_tower.on_for_degrees(SpeedPercent(20), -4, brake=True, block=False)
+        self.scan_tower.on_for_degrees(SpeedPercent(6), -126, brake=True, block=True)
         self.leds.set_color("LEFT", "BLACK")
-        print("Angle: %4.0i Left: %10.2f" % (self.gyro_sensor.angle,self.ultrasonic_sensor.distance_centimeters))
+        #print("Angle: %4.0i Left: %10.2f" % (self.gyro_sensor.angle,self.ultrasonic_sensor.distance_centimeters))
 
     def turn_right(self):
         #0..-126
+        self.tower_position = TowerPosition.RIGHT
         self.leds.set_color("RIGHT", "RED")
-        self.scan_tower.on_for_degrees(SpeedPercent(20), 4, brake=True, block=False)
+        #self.scan_tower.on_for_degrees(SpeedPercent(20), 4, brake=True, block=False)
+        self.scan_tower.on_for_degrees(SpeedPercent(6), 126, brake=True, block=True)
         self.leds.set_color("RIGHT", "BLACK")
-        print("Angle: %4.0i Right: %10.2f" % (self.gyro_sensor.angle,self.ultrasonic_sensor.distance_centimeters))
+        #print("Angle: %4.0i Right: %10.2f" % (self.gyro_sensor.angle,self.ultrasonic_sensor.distance_centimeters))
+        
+    def print_values(self):
+        while True:
+            if self.tower_position != TowerPosition.CENTER:
+                print("Angle: %4.0i %s: %10.2f" % (self.gyro_sensor.angle,self.tower_position,self.ultrasonic_sensor.distance_centimeters_continuous))
+                sleep(0.1)
 
     def scan_field(self):
 
-        while self.gyro_sensor.angle < 90:
-            self.turn_right()
-            sleep(0.2)
+        #while self.gyro_sensor.angle < 90:
+        #    self.turn_right()
+        #    sleep(0.2)
             
 
-        #while self.gyro_sensor.angle != 0:
+        #while self.gyro_sensor.angle >= -90:
         #    self.turn_left()
-
-        while self.gyro_sensor.angle >= -90:
-            self.turn_left()
-            sleep(0.2)
+        #    sleep(0.2)
             
 
-        while self.gyro_sensor.angle <= 0:
-            self.turn_right()
-            sleep(0.2)
+        #while self.gyro_sensor.angle <= 0:
+        #    self.turn_right()
+        #    sleep(0.2)
+        # Start threads
+        threading.Thread(target=self.print_values, daemon=True).start()
+        self.turn_right()
+        self.turn_left()
+        self.turn_left()
+        self.turn_right()
+        self.tower_position = TowerPosition.CENTER
+        
 
         
 
